265878ac244029ffaf1b1b851c2667ff0b09f55c
Zeth Thomas <zeththomas89@gmail.com>
2025-01-30 11:52:47 -0800

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Zeth Thomas on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlashzillaTests/FlashzillaTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaTests.swift
+//  FlashzillaTests
+//
+//  Created by Zeth Thomas on 1/30/25.
+//
+
+import Testing
+@testable import Flashzilla
+
+struct FlashzillaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
